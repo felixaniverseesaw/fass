4bd637eb5a1bb25911f42e826dfff63cc2463d63
Joshua Soh <86475694+felixaniverseesaw@users.noreply.github.com>
2021-06-28 02:14:03 +0000

    basically almost everything

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+    var div = $('#dynamicheight');
+    var width = div.width();
+    
+    div.css('height', width);
+});
+
+document.getElementsByClassName("active").disabled = true;
