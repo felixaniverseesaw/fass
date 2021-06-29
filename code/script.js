351c27625d3693cc6e4d247a33a7c6816361054c
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-06-29 16:16:20 +0800

    nothing

--- code/script.js
+++ code/script.js
@@ -7,12 +7,12 @@ $(function() {
 
 document.getElementsByClassName("active").disabled = true;
 
-var currentdate = new Date(); 
-document.getElementById("time").innerHTML = currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();;
+var d = new Date();
+document.getElementById("time").innerHTML = d.getDate() + "/"
+                + (d.getMonth()+1)  + "/" 
+                + d.getFullYear() + " @ "  
+                + d.getHours() + ":"  
+                + d.getMinutes() + ":" 
+                + d.getSeconds();
 
 
