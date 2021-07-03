b863a645f4cec437cb74abaddf8f99d92fd101d9
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-07-03 20:14:33 +0800

    fixed issue #8 in code/script.js

--- code/script.js
+++ code/script.js
@@ -11,6 +11,4 @@ var date = new Date();
 document.getElementById("time").innerHTML = date.getDate() + "/"
                 + (date.getMonth() + 1)  + "/" 
                 + date.getFullYear() + " @ "  
-                + date.getHours() + ":"  
-                + date.getMinutes() + ":" 
-                + date.getSeconds();
+                + date.toLocaleTimeString();
