d480181b7a88833b76849b3b1852f2fb63ef5728
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-07-08 16:24:16 +0800

    format

--- code/script.js
+++ code/script.js
@@ -1,16 +1,22 @@
-$(function() {
-    var div = $('#dynamicheight');
-    var width = div.width();
-    
-    div.css('height', width);
+$(function () {
+	var div = $("#dynamicheight");
+	var width = div.width();
+
+	div.css("height", width);
 });
 
 document.getElementsByClassName("active").disabled = true;
 
 var date = new Date();
-document.getElementById("time").innerHTML = date.getDate() + "/"
-                + (date.getMonth() + 1)  + "/" 
-                + date.getFullYear() + " @ "  
-                + date.getHours() + ":"  
-                + date.getMinutes() + ":" 
-                + date.getSeconds();
+document.getElementById("time").innerHTML =
+	date.getDate() +
+	"/" +
+	(date.getMonth() + 1) +
+	"/" +
+	date.getFullYear() +
+	" @ " +
+	date.getHours() +
+	":" +
+	date.getMinutes() +
+	":" +
+	date.getSeconds();
