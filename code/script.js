fdc7ab86e3300352a73b9f9b101867f3022b2afa
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-07-12 15:32:23 +0800

    made code more readable

--- code/script.js
+++ code/script.js
@@ -8,15 +8,9 @@ $(function () {
 document.getElementsByClassName("active").disabled = true;
 
 var date = new Date();
-document.getElementById("time").innerHTML =
-	date.getDate() +
-	"/" +
-	(date.getMonth() + 1) +
-	"/" +
-	date.getFullYear() +
-	" @ " +
-	date.getHours() +
-	":" +
-	date.getMinutes() +
-	":" +
+document.getElementById("time").innerHTML = date.getDate() + "/" +
+	(date.getMonth() + 1) + "/" +
+	date.getFullYear() + " @ " +
+	date.getHours() + ":" +
+	date.getMinutes() + ":" +
 	date.getSeconds();
