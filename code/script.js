12a1ef864402342cd0fc83b1149d728d5b88c2d2
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-07-28 20:38:40 +0800

    test

--- code/script.js
+++ code/script.js
@@ -5,8 +5,6 @@ $(function () {
 	div.css("height", width);
 });
 
-document.getElementsByClassName("active").disabled = true;
-
 var date = new Date();
 document.getElementById("time").innerHTML = date.getDate() + "/" +
 	(date.getMonth() + 1) + "/" +
