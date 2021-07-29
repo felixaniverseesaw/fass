c5c849137510f17b6aa4643f05cf9e04a064d84f
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-07-29 21:54:41 +0800

    Revert "change months from numbers to word format"
    This reverts commit ed3a42e1d1d133b7bf59c59d202bcd2f82b5a3ea.

--- code/script.js
+++ code/script.js
@@ -8,50 +8,8 @@ $(function () {
 document.getElementsByClassName("active").disabled = true;
 
 var date = new Date();
-var month = ""
-switch (date.getMonth() + 1)
-{
-	case 1:
-		month = "jan";
-		break;
-	case 2:
-		month = "feb";
-		break;
-	case 3:
-		month = "mar";
-		break;
-	case 4:
-		month = "apr";
-		break;
-	case 5:
-		month = "may";
-		break;
-	case 6:
-		month = "jun";
-		break;
-	case 7:
-		month = "jul";
-		break;
-	case 8:
-		month = "aug";
-		break;
-	case 9:
-		month = "sep";
-		break;
-	case 10:
-		month = "oct";
-		break;
-	case 11:
-		month = "nov";
-		break;
-	case 12:
-		month = "dec";
-		break;
-	default:
-		break;
-}
-document.getElementById("time").innerHTML = date.getDate() + " "
-	month + " "
+document.getElementById("time").innerHTML = date.getDate() + "/" +
+	(date.getMonth() + 1) + "/" +
 	date.getFullYear() + " @ " +
 	date.getHours() + ":" +
 	date.getMinutes();
