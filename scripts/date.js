174f4960e86624e7eeee4321deb1ec8ea3f75d3e
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-09-26 15:20:37 +0800

    added comments to js

--- scripts/date.js
+++ scripts/date.js
@@ -1,9 +1,12 @@
-// append zeros to single-digit values so that they remain consistent with the date format.
+// Appends zeroes to single-digit values so that they remain consistent with the date format.
 function appendLeadingZeroes(n) {
 	return n <= 9 ? "0" + n : n;
 }
 
+// Initialises Date object for display.
 let currentDate = new Date();
+
+// Formats the Date object using the apppendLeadingZeroes function.
 let dateFormat =
 	appendLeadingZeroes(currentDate.getDate()) +
 	"/" +
@@ -17,4 +20,5 @@ let dateFormat =
 	":" +
 	appendLeadingZeroes(currentDate.getSeconds());
 
+// Displays the formatted Date object in the <p></p> object with id "time".
 document.getElementById("time").innerHTML = "last updated: " + dateFormat;
