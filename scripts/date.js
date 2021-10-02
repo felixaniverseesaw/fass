15746b5be2adbf4909f6770bc2b548d42ed41c98
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-10-02 09:05:09 +0800

    updated variable names

--- scripts/date.js
+++ scripts/date.js
@@ -1,5 +1,5 @@
 // Appends zeroes to single-digit values so that they remain consistent with the date format.
-function appendLeadingZeroes(n) {
+function insertZeroes(n) {
 	return n <= 9 ? "0" + n : n;
 }
 
@@ -7,18 +7,18 @@ function appendLeadingZeroes(n) {
 let currentDate = new Date();
 
 // Formats the Date object using the apppendLeadingZeroes function.
-let dateFormat =
-	appendLeadingZeroes(currentDate.getDate()) +
+let formattedDate =
+	insertZeroes(currentDate.getDate()) +
 	"/" +
-	appendLeadingZeroes(currentDate.getMonth() + 1) +
+	insertZeroes(currentDate.getMonth() + 1) +
 	"/" +
 	currentDate.getFullYear() +
 	" @ " +
-	appendLeadingZeroes(currentDate.getHours()) +
+	insertZeroes(currentDate.getHours()) +
 	":" +
-	appendLeadingZeroes(currentDate.getMinutes()) +
+	insertZeroes(currentDate.getMinutes()) +
 	":" +
-	appendLeadingZeroes(currentDate.getSeconds());
+	insertZeroes(currentDate.getSeconds());
 
 // Displays the formatted Date object in the <p></p> object with id "time".
-document.getElementById("time").innerHTML = "last updated: " + dateFormat;
+document.getElementById("time").innerHTML = "last updated: " + formattedDate;
