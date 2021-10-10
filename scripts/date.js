99afdb21e25767ad07f557994b284abb6d373914
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-10-10 09:47:12 +0800

    formatting overhaul for less data transfer

--- scripts/date.js
+++ scripts/date.js
@@ -1,24 +1,24 @@
 // Appends zeroes to single-digit values so that they remain consistent with the date format.
 function insertZeroes(n) {
-	return n <= 9 ? "0" + n : n;
+  return n <= 9 ? '0' + n : n
 }
 
 // Initialises Date object for display.
-let currentDate = new Date();
+let currentDate = new Date()
 
 // Formats the Date object using the apppendLeadingZeroes function.
 let formattedDate =
-	insertZeroes(currentDate.getDate()) +
-	"/" +
-	insertZeroes(currentDate.getMonth() + 1) +
-	"/" +
-	currentDate.getFullYear() +
-	" @ " +
-	insertZeroes(currentDate.getHours()) +
-	":" +
-	insertZeroes(currentDate.getMinutes()) +
-	":" +
-	insertZeroes(currentDate.getSeconds());
+  insertZeroes(currentDate.getDate()) +
+  '/' +
+  insertZeroes(currentDate.getMonth() + 1) +
+  '/' +
+  currentDate.getFullYear() +
+  ' @ ' +
+  insertZeroes(currentDate.getHours()) +
+  ':' +
+  insertZeroes(currentDate.getMinutes()) +
+  ':' +
+  insertZeroes(currentDate.getSeconds())
 
 // Displays the formatted Date object in the <p></p> object with id "time".
-document.getElementById("time").innerHTML = "last updated: " + formattedDate;
+document.getElementById('time').innerHTML = 'last updated: ' + formattedDate
