efb8399f0592d8dd90d9aaa9a5de43bc96674e6c
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2021-11-04 15:45:49 +0800

    updated 2 spaces to size 2 tabs

--- scripts/date.js
+++ scripts/date.js
@@ -1,6 +1,6 @@
 // Appends zeroes to single-digit values so that they remain consistent with the date format.
 function insertZeroes(n) {
-  return n <= 9 ? '0' + n : n
+	return n <= 9 ? '0' + n : n
 }
 
 // Initialises Date object for display.
@@ -8,17 +8,17 @@ let currentDate = new Date()
 
 // Formats the Date object using the apppendLeadingZeroes function.
 let formattedDate =
-  insertZeroes(currentDate.getDate()) +
-  '/' +
-  insertZeroes(currentDate.getMonth() + 1) +
-  '/' +
-  currentDate.getFullYear() +
-  ' @ ' +
-  insertZeroes(currentDate.getHours()) +
-  ':' +
-  insertZeroes(currentDate.getMinutes()) +
-  ':' +
-  insertZeroes(currentDate.getSeconds())
+	insertZeroes(currentDate.getDate()) +
+	'/' +
+	insertZeroes(currentDate.getMonth() + 1) +
+	'/' +
+	currentDate.getFullYear() +
+	' @ ' +
+	insertZeroes(currentDate.getHours()) +
+	':' +
+	insertZeroes(currentDate.getMinutes()) +
+	':' +
+	insertZeroes(currentDate.getSeconds())
 
 // Displays the formatted Date object in the <p></p> object with id "time".
 document.getElementById('time').innerHTML = 'last updated: ' + formattedDate
